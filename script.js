7159a01eb05d669fcc89d950bd61e7ce9df3215f
das-Sumyadeep <133164855+das-Sumyadeep@users.noreply.github.com>
2024-04-23 13:42:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+function isAsciiOnly(str) {
+  for (var i = 0; i < str.length; i++)
+    if (str.charCodeAt(i) > 127)
+      return false;
+  return true;
+}
+
 const byteSize = (str) => {
   // write your code here
-	return str.length;
+	if(isAsciiOnly(str)) return str.length;
+	else
+		return str.length * 3;
 };
 
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
+
+
+
+
+
+
+
+
 
 
